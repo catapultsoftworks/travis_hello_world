0a846379d2e03064e6f2202193e019d5742b0cfe
catapultsoftworks <brich@Brandons-MacBook-Pro-2.local>
2013-11-18 21:53:59 -0500

    initial revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravisHelloWorld::Application.config.secret_key_base = '8ca5df681c99562359588819bf6773b6e83166ca40ab9ec26b2b06dcde97a71be6698a6f795cc0f4f1d8e27040e9532c9d05a420233d0d8ab66143c152de4b27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TravisHelloWorld::Application.config.session_store :cookie_store, key: '_travis_hello_world_session'
